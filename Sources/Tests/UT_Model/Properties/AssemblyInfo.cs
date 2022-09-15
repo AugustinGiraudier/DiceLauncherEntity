57c07cba4106ee7cd44f2b006251cefcf89cdc17
Augustin GIRAUDIER <augiraudie>
2022-09-15 11:30:35 +0200

    nouvelle solution

---
+++ Sources/Tests/UT_Model/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UT_Model")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("workgroup")]
+[assembly: AssemblyProduct("UT_Model")]
+[assembly: AssemblyCopyright("Copyright © workgroup 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bec3ffc7-5741-4cae-84e0-783a77546c9b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
