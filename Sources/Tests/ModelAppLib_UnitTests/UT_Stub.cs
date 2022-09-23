b1c2ad25094a57b1d723cebc9dadd35c6baef60c
Augustin Giraudier <giraudieraugustin@gmail.com>
2022-09-23 10:25:06 +0200

    ajout UT class Stub

---
+++ Sources/Tests/ModelAppLib_UnitTests/UT_Stub.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using ModelAppLib;
+using StubLib;
+using Xunit;
+
+namespace ModelAppLib_UnitTests
+{
+    public class UT_Stub
+    {
+        [Fact]
+        public void CreateObjectNotNull()
+        {
+            var stub = new Stub();
+            Assert.NotNull(stub);
+        }
+
+        [Fact]
+        public void DiceCollectionNotEmpty()
+        {
+            var stub = new Stub();
+            Assert.NotEmpty(stub.GetAllDices());
+        }
+
+        [Fact]
+        public void SideCollectionNotEmpty()
+        {
+            var stub = new Stub();
+            Assert.NotEmpty(stub.GetAllSides());
+        }
+
+
+    }
+}
