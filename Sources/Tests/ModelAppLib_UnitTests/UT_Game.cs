2819e3949dcf8c1bd819ca3e9b88ea03ddb5f03c
valentin cabrin <valentin.cabrin@cideco.tech>
2022-09-29 11:00:00 +0200

    création de tests pour la class Game

--- Sources/Tests/ModelAppLib_UnitTests/UT_Game.cs
+++ Sources/Tests/ModelAppLib_UnitTests/UT_Game.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using ModelAppLib;
 using Xunit;
@@ -12,6 +11,26 @@ namespace ModelAppLib_UnitTests;
         {
             Game gm = new Game(new List<DiceType>());
             Assert.NotNull(gm);
+        }
+
+        [Fact]
+        public void GetList()
+        {
+            List<DiceType> list = new List<DiceType>();
+            //Dice d = new(new DiceSideType(3, new DiceSide("img1")));
+            
+            Game gm = new Game(list);
+            Assert.Equal(new List<DiceType>(), gm.Dices);
+        }
+
+        [Fact]
+        public void TryAddDices()
+        {
+            List<DiceType> list = new List<DiceType>();
+            Game gm = new Game(list);
+            gm.AddDiceType(new DiceType(2, new Dice(new DiceSideType(3, new DiceSide("img1")))));
+            Assert.NotNull(gm.Dices);
+            Assert.Equal(list[0], gm.Dices[0]);
         }
 
         
